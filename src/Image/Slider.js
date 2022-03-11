df6914c218f2847f67f7e06ddd9cb716bb487613
vinay-vinny <vinayjadapalli599@gmail.com>
2022-03-11 14:57:55 +0530

    first-blog

---
+++ src/Image/Slider.js
@@ -0,0 +1,48 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "./Slider.css"
+
+const handleOnDragStart = e => e.preventDefault();
+
+function Slider() {
+  return (
+    <div className="slider">
+      <AliceCarousel>
+        <div>
+          <img
+            src="http://placekitten.com/200/300"
+            onDragStart={handleOnDragStart}
+            className="yours-custom-class"
+            alt="/"
+          />
+        </div>
+        <img
+          src="http://placekitten.com/200/400"
+          onDragStart={handleOnDragStart}
+          className="yours-custom-class"
+          alt="/"
+        />
+        <img
+          src="http://placekitten.com/200/304"
+          onDragStart={handleOnDragStart}
+          className="yours-custom-class"
+        />
+        <img
+          src="http://placekitten.com/200/301"
+          onDragStart={handleOnDragStart}
+          className="yours-custom-class"
+          alt="/"
+        />
+        <img
+          src="http://placekitten.com/200/302"
+          onDragStart={handleOnDragStart}
+          className="yours-custom-class"
+          alt="/"
+        />
+      </AliceCarousel>
+    </div>
+  );
+}
+
+export default Slider;
